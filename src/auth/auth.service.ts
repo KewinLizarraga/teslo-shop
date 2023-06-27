b68097fca327b70109a8b0f1ed5168ba9c76d492
kLizarraga <kewin392@gmail.com>
2023-06-27 16:52:02 -0500

    encrypt password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,6 +7,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -16,11 +17,18 @@ export class AuthService {
   ) {}
 
   async create(createUserDto: CreateUserDto) {
+    const { password, ...userData } = createUserDto;
+
     try {
-      const user = this.userRepository.create(createUserDto);
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
       await this.userRepository.save(user);
+      delete user.password;
 
       return user;
+      // TODO: retornar el JWT de acceso
     } catch (error) {
       this.handleDBErrors(error);
     }
