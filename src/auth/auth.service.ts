5222d8243358b0bc7982bdaa3ec4e095a47ff93f
kLizarraga <kewin392@gmail.com>
2023-06-30 10:32:50 -0500

    config jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,15 @@
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -32,6 +34,23 @@ export class AuthService {
     } catch (error) {
       this.handleDBErrors(error);
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+    if (!user)
+      throw new UnauthorizedException('Credentials are not valid (email)');
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Credentials are not valid (password)');
+
+    return user;
+    // TODO: retornar JWT
   }
 
   private handleDBErrors(error: any): never {
---
+++ src/auth/interface/jwt-payload.interface.ts
@@ -0,0 +1,4 @@
+export interface JwtPayload {
+  email: string;
+  // * Añadir información necesaria que debe tener el payload
+}
