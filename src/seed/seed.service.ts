b87733a26e3c46fe5abe3723831cf291e5a611f4
kLizarraga <kewin392@gmail.com>
2023-07-04 12:21:17 -0500

    encrypt password

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,49 @@
 import { Injectable } from '@nestjs/common';
-import { ProductsService } from 'src/products/products.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ProductsService } from '../products/products.service';
 import { initialData } from './data/seed-data';
+import { User } from '../auth/entities/user.entity';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly productService: ProductsService) {}
+  constructor(
+    private readonly productService: ProductsService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
   async runSeed() {
-    this.insertNewProducts();
+    await this.deleteTables();
+    const adminUser = await this.insertUsers();
+    await this.insertNewProducts(adminUser);
     return `Seed executed`;
   }
 
-  private async insertNewProducts() {
-    this.productService.deleteAllProducts();
+  private async deleteTables() {
+    await this.productService.deleteAllProducts();
+    const queryBuilder = this.userRepository.createQueryBuilder();
+    await queryBuilder.delete().where({}).execute();
+  }
 
+  private async insertUsers() {
+    const seedUsers = initialData.users;
+    const users: User[] = [];
+
+    seedUsers.forEach((user) => {
+      users.push(this.userRepository.create(user));
+    });
+
+    const dbUsers = await this.userRepository.save(seedUsers);
+    return dbUsers[0];
+  }
+
+  private async insertNewProducts(user: User) {
+    this.productService.deleteAllProducts();
     const products = initialData.products;
 
-    // await Promise.all(
-    //   products.map((product) => this.productService.create(product)),
-    // );
+    await Promise.all(
+      products.map((product) => this.productService.create(product, user)),
+    );
 
     return true;
   }
