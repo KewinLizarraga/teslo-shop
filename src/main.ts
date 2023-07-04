6d71461ad3bd0bee4ecf26cd0cb0ae4c23d6b7f8
kLizarraga <kewin392@gmail.com>
2023-07-04 16:45:01 -0500

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger('Bootstrap')
+  const logger = new Logger('Bootstrap');
 
   app.setGlobalPrefix('api');
 
@@ -15,8 +16,16 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT);
-  logger.log(`App running on port ${process.env.PORT}`)
+  const config = new DocumentBuilder()
+    .setTitle('Teslo API RESTful')
+    .setDescription('Teslo Shop endpoints')
+    .setVersion('1.0')
+    .build();
 
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
